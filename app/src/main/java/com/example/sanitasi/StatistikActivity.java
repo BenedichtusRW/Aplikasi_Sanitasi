ee70bb55ae99fb44b47098290d7c1d63a419c9df
claudia cyntia bella <claudia79pku@gmail.com>
2025-07-07 20:16:06 -0700

    dinasterkaitstatistik

---
+++ app/src/main/java/com/example/sanitasi/StatistikActivity.java
@@ -0,0 +1,14 @@
+package com.example.sanitasi;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StatistikActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistik);
+
+        // Tambahkan aksi klik atau fungsi lainnya jika perlu
+    }
+}
